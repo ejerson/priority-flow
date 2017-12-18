21f00ba5aaf7445e4f3fb3924fc4b1c82ab46423
ejerson <ejersonb@gmail.com>
2017-12-17 20:16:05 -0700

    Added three fragments for calendar tracker, priority slot, and goals.

--- app/src/main/java/com/eb/grow/priorityflow/PriorityActivity.java
+++ app/src/main/java/com/eb/grow/priorityflow/PriorityActivity.java
@@ -1,13 +1,30 @@
 package com.eb.grow.priorityflow;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import com.eb.grow.priorityflow.fragments.CalendarTrackerFragment;
+import com.eb.grow.priorityflow.fragments.GoalsFragment;
+import com.eb.grow.priorityflow.fragments.PrioritySlotFragment;
 
-public class PriorityActivity extends AppCompatActivity {
+public class PriorityActivity extends AppCompatActivity
+    implements CalendarTrackerFragment.OnFragmentInteractionListener,
+      GoalsFragment.OnFragmentInteractionListener,
+      PrioritySlotFragment.OnFragmentInteractionListener {
+
+  FragmentManager manager = getSupportFragmentManager();
+
+  PrioritySlotFragment priorityFragment = (PrioritySlotFragment) manager
+      .findFragmentById(R.id.fragment_container);
+  CalendarTrackerFragment trackerFragment = (CalendarTrackerFragment) manager
+      .findFragmentById(R.id.fragment_container);
+  GoalsFragment goalsFragment = (GoalsFragment) manager
+      .findFragmentById(R.id.fragment_container);
 
   private TextView mTextMessage;
 
@@ -18,13 +35,19 @@ public class PriorityActivity extends AppCompatActivity {
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
       switch (item.getItemId()) {
         case R.id.navigation_home:
-          mTextMessage.setText(R.string.title_home);
+              priorityFragment = new PrioritySlotFragment();
+          manager.beginTransaction().replace(R.id.fragment_container, priorityFragment)
+              .addToBackStack("Priority Slot").commit();
           return true;
         case R.id.navigation_dashboard:
-          mTextMessage.setText(R.string.title_dashboard);
+          trackerFragment = new CalendarTrackerFragment();
+          manager.beginTransaction().replace(R.id.fragment_container, trackerFragment)
+              .addToBackStack("Priority Slot").commit();
           return true;
         case R.id.navigation_notifications:
-          mTextMessage.setText(R.string.title_notifications);
+          goalsFragment = new GoalsFragment();
+          manager.beginTransaction().replace(R.id.fragment_container, goalsFragment)
+              .addToBackStack("Priority Slot").commit();
           return true;
       }
       return false;
@@ -37,9 +60,13 @@ public class PriorityActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_priority);
 
-    mTextMessage = (TextView) findViewById(R.id.message);
+
     BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
     navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
   }
 
+  @Override
+  public void onFragmentInteraction(Uri uri) {
+    // TODO What can I do with this method?
+  }
 }
